0c56ec29118e9695f7e46bde9902013feb3711f3
Aeius <87006912+Aeius@users.noreply.github.com>
2023-02-24 15:38:58 +0900

    Create dept.sql

---
+++ dept.sql
@@ -0,0 +1,12 @@
+create database lecture;
+use lecture;
+create table dept(
+        detpno int,
+        dname varchar(10),
+        loc varchar(10) 
+);
+insert into dept values(1111,'user1', 'test');
+insert into dept values(2222,'user2', 'test');
+insert into dept values(3333,'user3', 'test');
+insert into dept values(4444,'user4', 'test');
+commit;
